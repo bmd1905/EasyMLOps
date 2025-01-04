f6f7b81c3c4471a82659941a6231b491eb574070
Minh-Duc Bui <minhduc2k31905@gmail.com>
2025-01-05 00:36:23 +0700

    feat(cdc): Add CDC service with PostgreSQL integration and data handling
    - Introduced a new `cdc-python` service in `docker-compose.cdc.yaml` with dependencies on a PostgreSQL service, enhancing the data pipeline architecture.
    - Created a `Dockerfile` for the CDC service, including necessary dependencies and setup for running the application.
    - Added `requirements.txt` to manage Python dependencies for the CDC service, ensuring all required packages are installed.
    - Implemented data loading from a Parquet file in `insert_data.py`, improving data handling capabilities.
    - Updated `create_table.py` and `insert_data.py` to include the PostgreSQL host configuration, enhancing database connectivity.
    - Added a new `start.sh` script to streamline the initialization of the CDC service, including connector registration and data insertion processes.

--- src/cdc/run.sh
+++ src/cdc/run.sh
@@ -23,8 +23,7 @@ case $cmd in
             exit 1
         else
             echo "Registering a new connector from $2"
-            # Assign a connector config path such as: kafka_connect_jdbc/configs/connect-timescaledb-sink.json
-            curl -i -X POST -H "Accept:application/json" -H 'Content-Type: application/json' http://localhost:8088/connectors -d @$2
+            curl -i -X POST -H "Accept:application/json" -H 'Content-Type: application/json' http://kafka-connect:8083/connectors -d @$2
         fi
         ;;
     *)
